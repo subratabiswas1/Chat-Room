8bf3bf92f1bdcfdc74a63a2747d2bb544171ed22
subratabiswas1 <biswass83803@gmail.com>
2024-03-06 14:37:41 +0530

    ch

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ let $ = jQuery;
 let socket;
 
 function initializeWebSocket() {
-  socket = new WebSocket('ws://chat-room-3-nr79.onrender.com/message');
+  socket = new WebSocket('wss://chat-room-3-nr79.onrender.com/message');
 
   socket.onopen = function (event) {
     socket.send(JSON.stringify({"message": "Have joined!!","username": $('#usernameInput').val()}));
